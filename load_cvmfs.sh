3cc39aad486f2d106e3e90e2ef415954727b1655
Rasmus F. Ørsøe <48880272+RasmusOrsoe@users.noreply.github.com>
2021-01-12 10:05:14 +0100

    Add files via upload

---
+++ load_cvmfs.sh
@@ -0,0 +1,3 @@
+#eval `/cvmfs/icecube.opensciencegrid.org/py2-v3.1.1/setup.sh`
+eval `/cvmfs/icecube.opensciencegrid.org/py3-v4/setup.sh`
+/cvmfs/icecube.opensciencegrid.org/users/Oscillation/software/oscNext_meta/releases/latest/build/env-shell.sh
